d0fe79befb974252c86432233c1cdda99ba2fca3
sneharatnani <sneharatnani5@gmail.com>
2022-06-02 14:26:12 +0530

    ''

--- src/components/EmptyCart.js
+++ src/components/EmptyCart.js
@@ -3,7 +3,7 @@ import StyledLink from "./StyledLink.js";
 export default function EmptyCart() {
   return (
     <div
-      className="text-gray-800 w-full text-center absolute left-1/2 top-1/2 transform 
+      className="text-gray-800 w-full px-2 text-center absolute left-1/2 top-1/2 transform 
         -translate-x-1/2 -translate-y-1/2"
     >
       <p className="font-black text-3xl">Your Shopping Cart Is Empty</p>
